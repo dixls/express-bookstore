6ea51aab47cae2352f5e2e92d86105026b8019e5
pixls <tha.pixls@gmail.com>
2022-07-26 14:24:13 -0400

    added tests for post route

--- __tests__/bookRoutes.tests.js
+++ __tests__/bookRoutes.tests.js
@@ -26,10 +26,44 @@ describe("Book routes tests", () => {
         test("should return a list containing 1 book", async () => {
             let result = await request(app).get("/books");
 
-            expect(result.status).toBe(200);
+            expect(result.statusCode).toBe(200);
             expect(result.body).toEqual({ books: [testBook] });
         });
     });
+    describe("POST /books", () => {
+        test("should return successfully", async () => {
+            const successfulTestBook = {
+                isbn: 'testIsbn573',
+                amazon_url: 'http://a.co/eobPtX2',
+                author: 'test author2',
+                language: 'French',
+                pages: 538,
+                publisher: 'test publisher2',
+                title: 'test book2',
+                year: 2003
+            }
+            let result = await request(app)
+                .post("/books")
+                .send(successfulTestBook);
+            expect(result.statusCode).toBe(201);
+            expect(result.body).toEqual({book: successfulTestBook})
+        });
+        test("should return an error", async () => {
+            const incompleteBook = {
+                isbn: 'testIsbn3912',
+                amazon_url: 'http://a.co/eobPtX2',
+                author: 'test author2',
+                language: 'French',
+                pages: 538,
+                title: 'test book2',
+                year: 2003
+            }
+            let result = await request(app)
+                .post("/books")
+                .send(incompleteBook);
+            expect(result.statusCode).toBe(400);
+        })
+    })
 
 });
 
